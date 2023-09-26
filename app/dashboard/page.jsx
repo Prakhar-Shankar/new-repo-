4af4239d087927ff905db73637fe9b2c40b8c496
Prakhar-Shankar <prakharshankar247@gmail.com>
2023-09-26 11:04:32 +0530

    first commit

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <>
+    <h1 className="text-2xl font-bold ">Dashboard</h1>
+    <p>Welcome to the Dashboard</p>
+    </>
+  )
+}
+
+export default DashboardPage
